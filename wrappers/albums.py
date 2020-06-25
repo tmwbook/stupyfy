133e1a8a2ff00b0ef29b9d003fd5876405b520e1
Tom White <twhite@wpi.edu>
2020-06-24 22:15:36 -0400

    Add params to albums

--- wrappers/albums.py
+++ wrappers/albums.py
@@ -4,14 +4,25 @@ from ..api_utils import API_BASE, _get, api_call
 
 
 @api_call([404])
-def get_album(album_id: str):
-    return _get(f"{API_BASE}/albums/{album_id}")
+def get_album(album_id: str, market: str = None):
+    params = {
+        "market": market,
+    }
+    return _get(f"{API_BASE}/albums/{album_id}", params=params)
 
 @api_call([404])
-def get_album_tracks(album_id: str):
-    return _get(f"{API_BASE}/albums/{album_id}/tracks")
+def get_album_tracks(album_id: str, limit: int = None, offset: int = None, market: str = None):
+    params = {
+        "limit": limit,
+        "offset": offset,
+        "market": market,
+    }
+    return _get(f"{API_BASE}/albums/{album_id}/tracks", params=params)
 
 @api_call([404])
-def get_albums(album_ids: Sequence[str]):
-    # TODO(Tom): Does not yet support pagination
-    return _get(f"{API_BASE}/albums", params={"ids": album_ids})
+def get_albums(album_ids: Sequence[str], market: str = None):
+    params = {
+        "ids": album_ids,
+        "market": market,
+    }
+    return _get(f"{API_BASE}/albums", params=params)
