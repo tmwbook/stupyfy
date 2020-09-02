fe41319f45af5aa85f4f90eb63aba53aeb9ef001
Tom White <twhite@wpi.edu>
2020-09-02 18:49:28 -0400

    Structure move

--- setup.py
+++ setup.py
@@ -7,11 +7,14 @@ setuptools.setup(
     name="pyfy",
     version="0.0.1",
     author="Tom White",
-    author_email="twhite at wpi dot edu",
+    author_email="twhite@wpi.edu",
     description="A python wrapper for the Spotify API",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/tmwbook/pyfy",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'requests',
+    ],
     python_requires=">=3.6"
 )
